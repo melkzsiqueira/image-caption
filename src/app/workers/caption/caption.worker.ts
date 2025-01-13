091cc409a82ad1e0ce204322789959e741f996c0
Melkz Siqueira <melkz.siqueira@gmail.com>
2025-01-12 21:16:28 -0300

    feat: implement caption generation and model loading in Web Worker

--- src/app/workers/caption/caption.worker.ts
+++ src/app/workers/caption/caption.worker.ts
@@ -1,6 +1,85 @@
 /// <reference lib="webworker" />
 
-addEventListener('message', ({ data }) => {
-  const response = `worker response to ${data}`;
-  postMessage(response);
+import * as tf from '@tensorflow/tfjs';
+
+let model: tf.LayersModel | null = null;
+
+async function loadModel(file: string): Promise<void> {
+  model = await tf.loadLayersModel(file);
+}
+
+function preProcessImage(imgData: ImageData): tf.Tensor {
+  return tf.browser
+    .fromPixels(imgData)
+    .resizeNearestNeighbor([224, 224])
+    .toFloat()
+    .div(127.5)
+    .sub(1)
+    .expandDims();
+}
+
+async function generateCaption(imgData: ImageData): Promise<string> {
+  if (!model) {
+    throw new Error('Model is not loaded');
+  }
+
+  const preprocessedImage = preProcessImage(imgData);
+  const predictions = model.predict(preprocessedImage) as tf.Tensor;
+  const captionTokens = await predictions.array();
+
+  return tokensToCaption(captionTokens);
+}
+
+function tokensToCaption(
+  tokens:
+    | number
+    | number[]
+    | number[][]
+    | number[][][]
+    | number[][][][]
+    | number[][][][][]
+    | number[][][][][][]
+): string {
+  const vocabulary = ['<start>', 'a', 'cat', 'on', 'the', 'mat', '<end>'];
+  const flattenTokens = (data: any): number[] => {
+    if (Array.isArray(data)) {
+      return data.flatMap(flattenTokens);
+    }
+    return [data];
+  };
+  const flatTokens = flattenTokens(tokens);
+  const caption = flatTokens
+    .map((token) => vocabulary[token] || '')
+    .filter((word) => word && word !== '<start>' && word !== '<end>')
+    .join(' ');
+
+  return caption;
+}
+
+addEventListener('message', (event) => {
+  const { type, payload } = event.data;
+
+  switch (type) {
+    case 'LOAD_MODEL':
+      loadModel(payload)
+        .then(() => {
+          postMessage({
+            type: 'MODEL_LOADED',
+            message: 'Modelo carregado com sucesso!',
+          });
+        })
+        .catch((error) => {
+          postMessage({ type: 'MODEL_LOADED_ERROR', error: error.message });
+        });
+      break;
+    case 'GENERATE_CAPTION':
+      generateCaption(payload)
+        .then((caption) => {
+          postMessage({ type: 'CAPTION_GENERATED', caption });
+        })
+        .catch((error) => {
+          postMessage({ type: 'CAPTION_GENERATED_ERROR', error: error.message });
+        });
+      break;
+  }
 });
